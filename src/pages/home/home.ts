e330e67def8242f6fd8b600244cd2e5ae3ccb94d
Roberto Morales <razielku@users.noreply.github.com>
2017-04-10 01:27:00 -0300

    fix
    listas las funciones y el valor default

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,37 +9,81 @@ import { ApiService } from '../../app/services/api.service';
 })
 export class HomePage {
   
-  
-  private date : string;  
-  private value : number;
-  
+  /**
+  * fecha seleccionada por el usuario
+  **/
+  private date    :   string = new Date().toISOString();
+  /**
+  * valor UF en CLP del día seleccionado 
+  **/
+  private value   :   number;
+  /**
+  * numerico UF ingresado por usuario 
+  **/
+  private ufto    :   number;
+  /**
+  * numerico CLP ingresado por usuario
+  **/
+  private clpto   :   number;
+  /**
+  * Contenedor de resultado de  setUfToPesos
+  **/
+  private clp     :   number;
+  /**
+  * Contenedor de resultado de  setPesosToUF
+  **/
+  private uf      :   number;
+
   constructor(public navCtrl: NavController, private apiService : ApiService) {
-    this.date = '';
+    //this.date = '';
+    if(this.date!=''){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        var m = '';
+        var d = '';
+
+        d=(dd<10?'0':'')+dd;
+        m=(mm<10 ? '0':'')+mm;
+        var tday = yyyy+'-'+m+'-'+d;
+        this.date = tday;
+        this.onChangeDate();
+    }
   }
   
   //Obtener valor uf desde el ApiService
   onChangeDate(){
     
     //Formatear fecha
-    this.date = this.formatDate(this.date);
+    var day = this.formatDate(this.date);
     //Ir al api
-    this.apiService.getUfFromDate(this.date)
+    this.apiService.getUfFromDate(day)
         .subscribe((value) => {
           this.value = value.serie[0].valor;
-          console.log(this.value);
-        });     
+        });
+
   }
   
-
-  //Convertir cantidad de uf a pesos
-  setUfToPesos(cantidadUf : number, valorUf : number){
- 
-     return cantidadUf * valorUf;
+  /**
+  *cantidad de UF a CLP
+  */
+  setUfToPesos(event : any){
+    //console.log('setUfToPesos event', event);
+    //console.log(this, this.ufto);
+    this.clp = this.ufto * this.value;
+    if(isNaN(this.clp)) this.clp = null;
   }
 
-  //Convertir pesos to UF
-  setPesosToUF(cantidadPesos: number, valorUf:number){
-
+  /**
+  *cantidad de CPL a UF
+  */
+  setPesosToUF(event : any){
+    //console.log('setPesosToUF event', event);
+    //console.log(this, this.clpto);
+    this.uf = this.value != 0 ? this.clpto / this.value : 0;
+    if(isNaN(this.uf)) this.uf = null;
   }
 
 
